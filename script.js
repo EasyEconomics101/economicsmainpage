b1015e312c534882269ddee803530f257d5f42b1
EasyEconomics101 <econmadeeasy101@gmail.com>
2024-09-10 17:17:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let counterValue = 0;
 
+document.getElementById('increaseBtn').addEventListener('click', function() {
+  counterValue++;
+  document.getElementById('counter').innerText = counterValue;
+});
+
+document.getElementById('decreaseBtn').addEventListener('click', function() {
+  if (counterValue > 0) {
+    counterValue--;
+    document.getElementById('counter').innerText = counterValue;
+  }
+});
